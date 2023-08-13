80d25fc7ac1562e466c119acd732c473491bdced
Steven Kagumya <100422877+Kaigz@users.noreply.github.com>
2023-08-13 15:56:19 +0000

    corrected spelling of "numbers" in function

--- skills.js
+++ skills.js
@@ -1,4 +1,4 @@
-function calculateNumber(var1, var2) {
+function calculateNumbers(var1, var2) {
   const roundedVar1 = Math.round(var1);
   const roundedVar2 = Math.round(var2);
   return roundedVar1 + roundedVar2;
